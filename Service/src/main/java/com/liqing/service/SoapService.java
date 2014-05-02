8bb9aa5be80850e92bc5daf32f66c831350df676
liqing <cendy172@gmail.com>
2014-05-03 00:03:52 +0800

    invoke and test converting object to xml.

--- Service/src/main/java/com/liqing/service/SoapService.java
+++ Service/src/main/java/com/liqing/service/SoapService.java
@@ -1,9 +1,11 @@
 package com.liqing.service;
 
 import com.liqing.domain.StudentsListResponse;
+import com.liqing.service.soap.RequestBuilder;
 import com.liqing.service.soap.ServiceResponseReader;
 
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 
 public class SoapService
 {
@@ -15,7 +17,7 @@ public class SoapService
 
 		String xml = "<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\">"
 				+ "    <SOAP-ENV:Body>" + "        <listStudentsResp xmlns=\"urn:liqing:schema:com:student\">"
-				+ "            <responseHeader>" + "                <responseCreatedTime></responseCreatedTime>"
+				+ "            <responseHeader>" + "                <responseCreatedTime>1/5/2014</responseCreatedTime>"
 				+ "                <operationStatus>SUCCESS</operationStatus>" + "            </responseHeader>"
 				+ "            <students>" + "                <student id=\"1\">"
 				+ "                    <name>john</name>" + "                    <age>12</age>"
@@ -24,5 +26,17 @@ public class SoapService
 				+ "                </student>" + "            </students>" + "        </listStudentsResp>"
 				+ "    </SOAP-ENV:Body>" + "</SOAP-ENV:Envelope>";
 		return new ServiceResponseReader().getResult(xml, StudentsListResponse.class);
+	}
+
+	public String getStudentsXml()
+	{
+		final StudentsListResponse studentsList = createStudent();
+		try
+		{
+			return RequestBuilder.getStudentsRequest(studentsList);
+		} catch (UnsupportedEncodingException e)
+		{
+			throw new RuntimeException(e);
+		}
 	}
 }
--- Service/src/test/java/com/liqing/service/SoapServiceTest.java
+++ Service/src/test/java/com/liqing/service/SoapServiceTest.java
@@ -7,25 +7,39 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-public class SoapServiceTest {
+public class SoapServiceTest
+{
 
-    private SoapService soapService;
+	private SoapService soapService;
 
-    @Before
-    public void setUp() {
-        soapService = new SoapService();
-    }
+	@Before
+	public void setUp()
+	{
+		soapService = new SoapService();
+	}
 
-    @Test
-    public void shouldCreateStudentInstance() {
-        StudentsListResponse student = soapService.createStudent();
+	@Test
+	public void shouldCreateStudentInstance()
+	{
+		StudentsListResponse student = soapService.createStudent();
 
-        assertThat(student.getResponseHeader().getResponseCreatedTime(), is(""));
-        assertThat(student.getResponseHeader().getOperationStatus(), is("SUCCESS"));
-        assertThat(student.getStudents().size(), is(2));
-        assertThat(student.getStudents().get(0).getId(), is(1));
-        assertThat(student.getStudents().get(0).getName(), is("john"));
-        assertThat(student.getStudents().get(0).getAge(), is(12));
+		assertThat(student.getResponseHeader().getResponseCreatedTime(), is(""));
+		assertThat(student.getResponseHeader().getOperationStatus(), is("SUCCESS"));
+		assertThat(student.getStudents().size(), is(2));
+		assertThat(student.getStudents().get(0).getId(), is(1));
+		assertThat(student.getStudents().get(0).getName(), is("john"));
+		assertThat(student.getStudents().get(0).getAge(), is(12));
+	}
 
-    }
+	@Test
+	public void shouldGetXmlFromObject()
+	{
+		String xml = "<listStudentsResp>" + "<responseHeader>" + "<responseCreatedTime>1/5/2014</responseCreatedTime>"
+				+ "<operationStatus>SUCCESS</operationStatus>" + "</responseHeader>" + "<students>"
+				+ "<student id=\"1\">" + "<name>john</name>" + "<age>12</age>" + "</student>" + "<student id=\"2\">"
+				+ "<name>smith</name>" + "<age>13</age>" + "</student>" + "</students>" + "</listStudentsResp>";
+		final String studentsXml = soapService.getStudentsXml();
+
+		assertThat(studentsXml, is(xml));
+	}
 }
