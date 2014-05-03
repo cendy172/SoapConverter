271c0f53db21e990e8437b4f4ffdebfc676fcb75
liqing <cendy172@gmail.com>
2014-05-03 23:49:56 +0800

    fix order issue when creating xml.

--- Domain/src/main/java/com/liqing/domain/ResponseHeader.java
+++ Domain/src/main/java/com/liqing/domain/ResponseHeader.java
@@ -1,8 +1,13 @@
 package com.liqing.domain;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
 public class ResponseHeader
 {
 	private String responseCreatedTime;
+
 	private String operationStatus;
 
 	public String getResponseCreatedTime()
